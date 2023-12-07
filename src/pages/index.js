0e49c215850385694271cf41c24f2108d85f8e42
shkirfan67 <shkirfan67@gmail.com>
2023-12-07 18:22:36 +0530

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import Home from "./HomePage/HomePage";
+import Cart from "./CartPage/CartPage";
+import CategoryProduct from "./CategoryProductPage/CategoryProductPage";
+import ProductSingle from "./ProductSinglePage/ProductSinglePage";
+import Search from "./SearchPage/SearchPage";
+
+export {Home, CategoryProduct, ProductSingle, Cart, Search};
